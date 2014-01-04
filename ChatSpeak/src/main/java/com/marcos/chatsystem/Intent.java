f4ab515978ef426e2d8d3d89752530c7132aabef
MarcosNeco <marcosmattosjunior@gmail.com.br>
2014-01-04 19:06:46 -0200

    New version system

---
+++ ChatSpeak/src/main/java/com/marcos/chatsystem/Intent.java
@@ -0,0 +1,26 @@
+package com.marcos.chatsystem;
+
+import java.io.IOException;
+
+public class Intent implements Appendable {
+
+	@Override
+	public Appendable append(CharSequence arg0) throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Appendable append(char arg0) throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Appendable append(CharSequence arg0, int arg1, int arg2)
+			throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
